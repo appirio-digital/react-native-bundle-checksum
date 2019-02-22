10c02da47bc8b72378de3061d98600fbf888c462
Brandon Jones <bjones@appirio.com>
2019-02-22 13:12:10 -0500

    Fix to ensure that Android Hash retrieves file in the same manner as we get in iOS.

--- android/src/main/java/com/bundlechecksum/RNBundleChecksumModule.java
+++ android/src/main/java/com/bundlechecksum/RNBundleChecksumModule.java
@@ -2,6 +2,7 @@
 package com.bundlechecksum;
 
 import android.content.res.AssetManager;
+import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -9,11 +10,15 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
 
@@ -33,55 +38,50 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
   public void getChecksum(Promise promise){
     BufferedReader br = null;
     String bundle = null;
+    StringBuilder sb = new StringBuilder();
     try {
       AssetManager assetManager = getReactApplicationContext().getAssets();
-      InputStream stream = assetManager.open("index.android.bundle", 0);
+      InputStream stream = assetManager.open("index.android.bundle");
       if (stream == null) {
-        reject(promise, "com.bundlechecksum", new Exception("Failed to open bundle file. You may be running in development mode."));
+        promise.reject("com.bundlechecksum", new Exception("Failed to open bundle file. You may be running in development mode."));
         return;
       }
 
-      br = BufferedReader(new InputStreamReader(stream, "UTF-8"));
-      StringBuilder sb = new StringBuilder();
-
-      String str;
-      while ((str = br.read()) != null){
-        sb.append(str);
-      }
-      br.close();
-
-      bundle = sb.toString();
-      sb = null;
+      int size = stream.available();
+      byte[] buffer = new byte[size];
+      stream.read(buffer);
+      stream.close();
+      bundle = new String(buffer);
 
       if (bundle == null || bundle.equals("")) {
-        reject(promise, "com.bundlechecksum", new Exception("Failed to open bundle file. You may be running in development mode."));
+        promise.reject("com.bundlechecksum", new Exception("Failed to open bundle file. You may be running in development mode."));
         return;
       }
     } catch (Exception ex) {
       ex.printStackTrace();
-      reject(promise, "com.bundlechecksum", ex);
-    } finally {
-      if (br != null) {
-        try {
-          br.close();
-        } catch (IOException e){
-          Log.e("com.bundlechecksum", "Error closing stream.");
-        }
-      }
+      promise.reject("com.bundlechecksum", ex);
     }
 
     MessageDigest md = null;
+    sb = new StringBuilder();
     try {
         md = MessageDigest.getInstance("SHA-256");
         md.update(bundle.getBytes("UTF-8"));
         byte[] digest = md.digest();
-        String hash = String.format("%064x", new java.math.BigInteger(1, digest));
-        promise.resolve(hash);
+        
+        for (int i = 0; i < digest.length; i++) {
+          sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        promise.resolve(sb.toString());
 
     } catch (NoSuchAlgorithmException e) {
         e.printStackTrace();
         promise.reject("com.bundlechecksum", e.getMessage());
     } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+        promise.reject("com.bundlechecksum", e.getMessage());
+    } catch (Exception e) {
         e.printStackTrace();
         promise.reject("com.bundlechecksum", e.getMessage());
     }
